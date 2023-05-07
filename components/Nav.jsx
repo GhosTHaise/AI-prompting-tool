5ebeb8b3d6e7dd7ca3a31d9d9cba0025529d2a05
GhosTHaise <Ghostrex2@gmail.com>
2023-05-07 17:30:41 +0300

    Build Navbar and init loggin providers

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,19 @@ import {useState,useEffect} from "react";
 import { signIn, signOut, useSession, getProviders} from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(()=>{
+    const setProviders_func = async () => {
+       const response = await getProviders();
+       setProviders(response);
+    }
+
+    setProviders_func();
+  },[]);
+
   return (
     <nav 
     className="flex-between w-full mb-16 pt-3">
@@ -20,7 +33,47 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
+        <p className="logo_text">
+          Promptopia
+        </p>
       </Link>
+
+      {/* Desktop Nav */}
+      <div className="sm:flex hidden">
+        {
+          isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+                <Link 
+                href="/create-prompt"
+                className="black_btn"
+                >
+                  Create Post
+                </Link>
+                <button 
+                type="button"
+                className="outline_btn"
+                onClick={signOut}
+                >
+                  Sign Out
+                </button>
+
+                <Link href="/profile">
+                  <Image 
+                    src="/assets/images/logo.svg"
+                    width={37}
+                    height={37}
+                    className="rounded-full"
+                    alt="profile"
+                  />
+                </Link>
+            </div>
+          ) : (
+            <>
+              
+            </>
+          )
+        }
+      </div>
     </nav>
   )
 }
