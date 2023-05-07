23a7e233c8b3fdb5a66ac2026b5ddb2eb14fc475
GhosTHaise <Ghostrex2@gmail.com>
2023-05-07 17:15:25 +0300

    Insert Navbar Logo

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-
+import Feed from "@components/Feed"
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -13,7 +13,7 @@ const Home = () => {
         and share creative prompts
       </p>
 
-      {/* Feed component */}
+      <Feed />
     </section>
   )
 }
