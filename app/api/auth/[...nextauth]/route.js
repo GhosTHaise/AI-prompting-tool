864a52535d90deec65ab7c74565bbe3ae8902e41
GhosTHaise <Ghostrex2@gmail.com>
2023-05-09 21:22:06 +0300

    connect DB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 
+import { connectToDB } from "@utils/database";
 /* console.log({
     clientId : process.env.GOOGLE_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET
@@ -17,9 +18,16 @@ const handler = NextAuth({
     },
     async signIn({profile}){
         try {
-            
+            await connectToDB();
+
+            //check if a user already exists
+
+            //if not, create a new user
+
+            return true;
         } catch (error) {
-            
+            console.log(error)
+            return false;
         }
     }
 });
