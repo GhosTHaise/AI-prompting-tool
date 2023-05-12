ab84b7de666fb228615f8e8397dcb10337c51ab8
GhosTHaise <Ghostrex2@gmail.com>
2023-05-12 20:52:48 +0300

    Init Create Post page.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -9,9 +9,23 @@ const Form = ({
 }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
-        <h1>
-            {type} Post
+        <h1 className="head_text text-left">
+            <span className="blue_gradient">
+              {type} Post
+            </span>
         </h1>
+        <p className="desc text-left max-w-md ">
+            {type} and share amazing prompts with the 
+            world, and let your imagination run wild with 
+            any AI-powered platform.
+        </p>
+        <form 
+          className="mt-10 w-full max-z-2xl 
+          flex flex-col gap-7 glassmorphism"
+          onSubmit={handleSubmit}
+          >
+
+          </form>
     </section>
   )
 }
