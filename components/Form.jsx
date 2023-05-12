2a3ef518da958c5595576f0c0f62ffd59fad115b
GhosTHaise <Ghostrex2@gmail.com>
2023-05-12 21:25:05 +0300

    Create forn on my create-post page

--- components/Form.jsx
+++ components/Form.jsx
@@ -24,7 +24,42 @@ const Form = ({
           flex flex-col gap-7 glassmorphism"
           onSubmit={handleSubmit}
           >
-
+            <label className="">
+                <span className="font-satoshi font-semibold 
+                text-base text-gray-700" >
+                  Your AI Prompt
+                </span>
+                <textarea
+                  value={post.pompt}
+                  onChange={(e) => setPost(
+                  { 
+                    ...post , 
+                    prompt : e.target.value
+                  })}
+                  placeholder="Write your prompt here..."
+                  required
+                  className="form_textarea"
+                />
+            </label>
+            
+            <label className="">
+                <span className="font-satoshi font-semibold 
+                text-base text-gray-700" >
+                  Tag <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+                </span>
+                <input
+                  value={post.tag}
+                  onChange={(e) => setPost(
+                  { 
+                    ...post , 
+                    tag : e.target.value
+                  })}
+                  placeholder="#tag"
+                  required
+                  className="form_input"
+                />
+            </label>
+            
           </form>
     </section>
   )
