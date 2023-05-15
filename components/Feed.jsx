48afb0a35c93191555610caeb41e1d4bf0ad877f
GhosTHaise <Ghostrex2@gmail.com>
2023-05-15 21:18:45 +0300

    Create Route to take all prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,10 +2,18 @@
 
 import {useState,useEffect } from 'react'
 import PromptCard from './PromptCard'
+
+
 const PromptCardList = ({data,handleTagClick}) => 
 <div className='mt-16 prompt_layout'>
     {
-      data
+      data.map((post)=> (
+        <PromptCard 
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))
     }
 </div>;
 
