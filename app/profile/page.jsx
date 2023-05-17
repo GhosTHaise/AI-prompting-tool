9f27ab77f84f8036cff0ad7f10cf1731a6be473c
GhosTHaise <Ghostrex2@gmail.com>
2023-05-17 23:07:18 +0300

    fetch userData using user session id

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,9 +4,27 @@ import { useState , useEffect } from 'react';
 import { useSession } from 'next-auth/react'; 
 import { useRouter } from 'next/router';
 
-import Profil from '@components/Profil';
+import MyProfil from '@components/Profil';
 
 const Profile = () => {
+
+  const {data : session} = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      try {
+        const response = await fetch(`/api/users/${session?.user.id}/posts`);
+        const data = await response.json();
+
+        setPosts(data);
+      } catch (error) {
+          console.log(error);
+      }
+    };
+
+    if(session?.user.id) fetchPosts();
+  },[]);
 
     const handleEdit = () => {
 
@@ -20,11 +38,11 @@ const Profile = () => {
     <Profile
         name="My"
         desc="Welcome to your personalized profil page"
-        data={[]}
+        data={posts}
         handleEdit={handleEdit}
         hanleDelete={handleDelete}
     />
   )
 }
 
-export default Profile
+export default MyProfile
